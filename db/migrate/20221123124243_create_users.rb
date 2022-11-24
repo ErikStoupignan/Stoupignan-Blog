455e436c0bd69dd86089230ec1d1b3ff7176e33c
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-11-24 14:01:07 -0600

    Add processing data models

--- db/migrate/20221123124243_create_users.rb
+++ db/migrate/20221123124243_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo
+      t.text :photo
       t.text :bio
-      t.string :posts_counter
+      t.integer :posts_counter
 
       t.timestamps
     end
