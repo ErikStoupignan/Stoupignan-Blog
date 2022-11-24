3b709b3bf57dcb90d68b2729af0046afbbab3367
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-11-24 16:47:02 -0600

    Apply changes required on the counters variables

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post, counter_cache: :comments_counter
+  belongs_to :post
+  after_save :update_total_comment
+
+  def update_total_comment
+    post.update(comments_counter: post.comments.all.length)
+  end
 end
