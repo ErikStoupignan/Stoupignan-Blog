9be15c7e2e737b11aa9cf3904f7320291d4d5cf4
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-11-30 21:46:40 -0600

    Add some css styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
 
   after_save :update_total_posts
 
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :like_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_five_comments
     comments.order(created_at: :desc).limit(5)
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# # Add Users
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+first_user = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1508922088233-a07c9cc6972f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', bio: 'Teacher from Poland.')
 
-# # Add four posts
 first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
 second_post = Post.create(author: first_user, title: 'This is my second post!', text: 'Second comment')
 third_post = Post.create(author: second_user, title: 'Hello everyone!', text: 'I am new on this')
 fourth_post = Post.create(author: second_user, title: 'Microverse', text: 'This is very cool')
 
-# Add 6 comments into the posts
-Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
 Comment.create(post: first_post, author: first_user, text: 'Hi Lilly, How was your day?')
 Comment.create(post: first_post, author: second_user, text: 'I am very well, thank you')
 Comment.create(post: second_post, author: second_user, text: 'Congrats for your second post')
