909eaf6b550f74510be758728840fd3ca925698f
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-12-06 16:18:24 -0600

    Correction in the configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  # Main link for the covert
   root 'users#index'
-
-  # get '/users', to: 'users#index'
-  # get '/users/:id', to: 'users#show', as: :user
-  # get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
-  # get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
